fac3b6e94838e14c141c7a045db775c781adc2c5
just nobody <kingdaro@gmail.com>
2017-08-27 02:02:51 -0400

    shoehorn app code into a class

---
+++ src/client/app.ts
@@ -0,0 +1,43 @@
+import * as io from 'socket.io-client'
+import { Point } from './point'
+import { DrawingBoard } from './drawing-board'
+import * as chat from './chat'
+
+export class App {
+  getCanvas() {
+    const canvas = document.querySelector('.game-canvas')
+    if (canvas instanceof HTMLCanvasElement) return canvas
+    throw new Error('Could not get canvas element')
+  }
+
+  init() {
+    const canvas = this.getCanvas()
+    const graphics = canvas.getContext('2d') as CanvasRenderingContext2D
+    const drawingBoard = new DrawingBoard()
+
+    canvas.addEventListener('pointerdown', event => {
+      drawingBoard.beginDrawing(new Point(event.offsetX, event.offsetY))
+    })
+
+    window.addEventListener('pointerup', () => {
+      drawingBoard.finishDrawing()
+    })
+
+    canvas.addEventListener('pointermove', event => {
+      if (drawingBoard.isPenDown) {
+        drawingBoard.updateCurrentLine(new Point(event.offsetX, event.offsetY))
+        drawingBoard.render(graphics)
+      }
+    })
+
+    canvas.addEventListener('keydown', event => {
+      if (event.ctrlKey && event.keyCode === 90) {
+        drawingBoard.undo()
+        drawingBoard.render(graphics)
+      }
+    })
+
+    const socket = io('localhost:3000')
+    chat.init(socket)
+  }
+}
